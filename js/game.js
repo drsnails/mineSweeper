4dbaa35d1ad1892e8ffd3e64e737325fff28b169
drsnails <55944667+drsnails@users.noreply.github.com>
2020-07-26 01:05:30 +0300

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -22,9 +22,9 @@ var gIsManual = false
 var gVisited;
 var gSafeClicksLeft;
 
-localStorage.setItem('level4', [])
-localStorage.setItem('level8', [])
-localStorage.setItem('level12', [])
+// localStorage.setItem('level4', [])
+// localStorage.setItem('level8', [])
+// localStorage.setItem('level12', [])
 
 
 var gLevel = {
@@ -83,7 +83,7 @@ function init() {
     gRecenGameStats = []
     initSafeClickBtn()
     renderBoard()
-    // renderBestScores('level4')
+    renderBestScores('level4')
 }
 
 
@@ -242,27 +242,27 @@ function gameOver(isWin) {
     var elRestart = document.querySelector('.restart-container p .restart')
     if (isWin) {
         elRestart.innerText = WIN
-        // if (!gIsUndo) {
-        //     switch (gLevel.SIZE) {
+        if (!gIsUndo) {
+            switch (gLevel.SIZE) {
     
-        //         case 4:
-        //             localStorage['level4'] += ` ${gGame.secsPassed}`
-        //             renderBestScores('level4')
-        //             break;
+                case 4:
+                    localStorage['level4'] += ` ${gGame.secsPassed}`
+                    renderBestScores('level4')
+                    break;
     
-        //         case 8:
-        //             localStorage['level8'] += ` ${gGame.secsPassed}`
-        //             renderBestScores('level8')
-        //             break;
+                case 8:
+                    localStorage['level8'] += ` ${gGame.secsPassed}`
+                    renderBestScores('level8')
+                    break;
     
-        //         case 12:
-        //             localStorage['level12'] += ` ${gGame.secsPassed}`
-        //             renderBestScores('level12')
+                case 12:
+                    localStorage['level12'] += ` ${gGame.secsPassed}`
+                    renderBestScores('level12')
     
-        //         default:
-        //             break;
-        //     }
-        // }
+                default:
+                    break;
+            }
+        }
 
     } else {
         elRestart.innerText = DEAD
@@ -331,17 +331,17 @@ function initManualGame() {
     gTimeInterval = setInterval(renderTime, 10)
 }
 
-// function renderBestScores(level) {
-//     var elScoresHead = document.querySelector('.scores-head span')
-//     var elOl = document.querySelector('ol')
-//     elScoresHead.innerText = `(${gLevel.SIZE}x${gLevel.SIZE})`
-//     var scoresArray = localStorage[level]
-//     scoresArray = scoresArray.split(' ')
-//     scoresArray.sort(function (a, b) { return parseFloat(a) - parseFloat(b) })
-//     elOl.innerHTML = ''
-//     for (var i = 1; i < scoresArray.length; i++) {
-//         elOl.innerHTML += `<li>  ${scoresArray[i]} sec`
-//         elOl.innerHTML += `</li> `
+function renderBestScores(level) {
+    var elScoresHead = document.querySelector('.scores-head span')
+    var elOl = document.querySelector('ol')
+    elScoresHead.innerText = `(${gLevel.SIZE}x${gLevel.SIZE})`
+    var scoresArray = localStorage[level]
+    scoresArray = scoresArray.split(' ')
+    scoresArray.sort(function (a, b) { return parseFloat(a) - parseFloat(b) })
+    elOl.innerHTML = ''
+    for (var i = 1; i < scoresArray.length; i++) {
+        elOl.innerHTML += `<li>  ${scoresArray[i]} sec`
+        elOl.innerHTML += `</li> `
 
-//     }
-// }
+    }
+}
